2e0f182305fa935c0963e7177d5a86f2f25af8b6
Jorge Medina <jorgemedinaleigh@protonmail.com>
2022-01-19 18:57:55 -0300

    Added 3rd person camera & level props

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public CharacterController controller;
+    public GameObject firstPersonCam;
+    public GameObject thirdPersonCam;
 
     void Update()
     {
         ProcessMovement();
+        CameraChange();
     }
     void ProcessMovement()
     {
@@ -19,5 +20,23 @@ public class PlayerController : MonoBehaviour
         Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move * moveSpeed * Time.deltaTime); 
+    }
+
+    void CameraChange()
+    {
+        if(Input.GetKeyDown(KeyCode.V))
+        {
+            if(firstPersonCam.activeSelf)
+            {
+                firstPersonCam.SetActive(false);
+                thirdPersonCam.SetActive(true);
+            }
+            else if(thirdPersonCam.activeSelf)
+            {
+                
+                thirdPersonCam.SetActive(false);
+                firstPersonCam.SetActive(true);
+            }
+        }
     }
 }
